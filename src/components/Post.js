3432b260b73d0255261280f699778f1e7ecbe55c
Martin Sanne Kristiansen <martin@heydays.no>
2019-05-21 23:05:36 +0200

    add posts

---
+++ src/components/Post.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react"
+import { Html, FeaturedImage, PostDate, ResponsiveEmbeds } from "./utils"
+import NewsCard from "./NewsCard"
+
+const Post = ({ post, posts }) => {
+  const { title, wordpress_id, content, featured_image, date, excerpt } = post
+
+  return (
+    <article className="Post">
+      {title && (
+        <header className="Post__header">
+          <h1 className="Post__title">
+            <Html content={title} />
+          </h1>
+          {date && (
+            <p className="Post__date">
+              <PostDate date={date} />
+            </p>
+          )}
+          {excerpt && (
+            <div className="Post__lead">
+              <Html content={excerpt} />
+            </div>
+          )}
+        </header>
+      )}
+      {featured_image && (
+        <FeaturedImage
+          className="Post__featured-image"
+          {...featured_image}
+          showCaption={true}
+        />
+      )}
+      <div className="Post__content">
+        <ResponsiveEmbeds>
+          <Html content={content} />
+        </ResponsiveEmbeds>
+      </div>
+      <footer className="Post__footer">
+        <div className="NewsCards NewsCards--footer">
+          {posts &&
+            posts
+              .filter(post => post.wordpress_id !== wordpress_id)
+              .slice(0, 4)
+              .map(post => (
+                <NewsCard key={`NewsCard-${post.wordpress_id}`} post={post} />
+              ))}
+        </div>
+      </footer>
+    </article>
+  )
+}
+
+export default Post
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,14 +1,121 @@
 import React from "react"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
+import Post from "../components/Post"
 
 export const pageQuery = graphql`
   query($id: String!) {
+    allWordpressPost(limit: 10, filter: { locale: { eq: "nb" } }) {
+      edges {
+        node {
+          title
+          wordpress_id
+          link
+          excerpt
+          locale
+          date
+          featured_image {
+            title
+            filename
+            filesize
+            url
+            link
+            alt
+            author
+            description
+            caption
+            name
+            status
+            uploaded_to
+            date
+            modified
+            menu_order
+            mime_type
+            type
+            subtype
+            icon
+            width
+            height
+            sizes {
+              thumbnail
+              thumbnail_width
+              thumbnail_height
+              medium
+              medium_width
+              medium_height
+              medium_large
+              medium_large_width
+              medium_large_height
+              large
+              large_width
+              large_height
+              small
+              small_width
+              small_height
+              medium_small
+              medium_small_width
+              medium_small_height
+              xlarge
+              xlarge_width
+              xlarge_height
+            }
+          }
+        }
+      }
+    }
+
     wordpressPost(id: { eq: $id }) {
-      title
-      date(formatString: "Do MMMM YYYY")
-      content
       wordpress_id
+      title
+      excerpt
+      content
+      date
+      featured_image {
+        title
+        filename
+        filesize
+        url
+        link
+        alt
+        author
+        description
+        caption
+        name
+        status
+        uploaded_to
+        date
+        modified
+        menu_order
+        mime_type
+        type
+        subtype
+        icon
+        width
+        height
+        sizes {
+          thumbnail
+          thumbnail_width
+          thumbnail_height
+          medium
+          medium_width
+          medium_height
+          medium_large
+          medium_large_width
+          medium_large_height
+          large
+          large_width
+          large_height
+          small
+          small_width
+          small_height
+          medium_small
+          medium_small_width
+          medium_small_height
+          xlarge
+          xlarge_width
+          xlarge_height
+        }
+      }
       translations {
         nb {
           url
@@ -23,16 +130,18 @@ export const pageQuery = graphql`
 
 export default ({ data, pageContext, translations, ...rest }) => {
   const post = data.wordpressPost
+  const posts = data.allWordpressPost.edges.map(item => item.node)
   const { locale } = pageContext
   return (
     <Layout translations={translations} locale={locale}>
-      <div>
+      <Post post={post} posts={posts} />
+      {/* <div>
         <p>Post:</p>
         {post.wordpress_id} - {locale}
         <h1 dangerouslySetInnerHTML={{ __html: post.title }} />
         <h3>date: {post.date}</h3>
         <div dangerouslySetInnerHTML={{ __html: post.content }} />
-      </div>
+      </div> */}
     </Layout>
   )
 }
