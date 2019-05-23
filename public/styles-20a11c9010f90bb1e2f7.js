6b950fac747b030039d5f15f28e993c4e7d6d018
Martin Sanne Kristiansen <martin@heydays.no>
2019-05-23 11:36:23 +0200

    remove public dir

--- public/styles-20a11c9010f90bb1e2f7.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{107:function(n,w,o){}}]);
-//# sourceMappingURL=styles-20a11c9010f90bb1e2f7.js.map
--- public/styles-37587c83d3eb3eba9dab.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{110:function(n,w,o){}}]);
-//# sourceMappingURL=styles-37587c83d3eb3eba9dab.js.map
