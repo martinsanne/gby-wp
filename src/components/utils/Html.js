afcb04e5cf7ed12b41bd35d5b79106a976b3ed57
mittnavnermike <mikepatterson@Mikes-MBP-2.heydays.local>
2019-10-07 14:25:50 +0200

    :sparkles: added stripStyleTags to Html

--- src/components/utils/Html.js
+++ src/components/utils/Html.js
@@ -1,12 +1,13 @@
 import React from "react"
 const stripHTMLTags = str => str.replace(/<[^>]*>/g, "")
+const stripStyleTags = str => str.replace(/style=('|")(.)*"/g, "")
 
 const Html = props => {
   let { stripTags, content } = props
   if (stripTags) {
     content = stripHTMLTags(content)
   }
-  return <div dangerouslySetInnerHTML={{ __html: content }} />
+  return <div dangerouslySetInnerHTML={{ __html: stripStyleTags(content) }} />
 }
 
 export default Html
