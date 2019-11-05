9053c65d94a4212b71f2776707380e66927de4d7
Martin Sanne Kristiansen <martin@heydays.no>
2019-11-05 15:13:11 +0100

    hero graphix

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,30 +6,34 @@ const Hero = ({ hero }) => {
   return (
     <div className="Hero">
       <div className="Hero__container container">
-        {hero.headliners && hero.headliners.length > 2 && (
-          <Illustration
-            className="Hero__illustration"
-            src="illustrations/carrot.png"
-          />
-        )}
-        {hero?.headliners && (
-          <HeadlinerArtists artists={hero.headliners} hero />
-        )}
-        {hero.headliners && hero.headliners.length <= 2 && (
-          <>
-            <p className="Hero__payoff">
-              Årets første artistslipp. Følg med for flere slipp snart!
-            </p>
+        <div className="Hero__content">
+          {hero.headliners && hero.headliners.length <= 2 && (
             <Illustration
               className="Hero__rocket"
               src="illustrations/rocket-left-speed.png"
             />
+          )}
+          {hero.headliners && hero.headliners.length > 2 && (
             <Illustration
-              className="Hero__hand"
-              src="illustrations/hero-shrooms.png"
+              className="Hero__illustration"
+              src="illustrations/carrot.png"
             />
-          </>
-        )}
+          )}
+          {hero?.headliners && (
+            <HeadlinerArtists artists={hero.headliners} hero />
+          )}
+          {hero.headliners && hero.headliners.length <= 2 && (
+            <>
+              <Illustration
+                className="Hero__hand"
+                src="illustrations/hero-shrooms.png"
+              />
+            </>
+          )}
+        </div>
+        <p className="Hero__payoff">
+          Årets første artistslipp. Følg med for flere slipp snart!
+        </p>
       </div>
     </div>
   )
