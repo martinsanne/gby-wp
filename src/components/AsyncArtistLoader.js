5c97f78abaddfbda8780c66f7b8a1a74c1f06007
mittnavnermike <mikepatterson@Mikes-MBP-2.heydays.local>
2019-12-10 13:29:24 +0100

    fix linter errors

--- src/components/AsyncArtistLoader.js
+++ src/components/AsyncArtistLoader.js
@@ -6,22 +6,22 @@ export default function AsyncArtistLoader({ pageId, children }) {
   const [artists, setArtists] = useState(null)
   const [hero, setHero] = useState(null)
 
-  const fetchData = () => {
-    axios.get(`${getApiURL()}/wp-json/wp/v2/pages/${pageId}`).then(res => {
-      if (res.data && res.data.acf) {
-        if (res.data.acf.artists) {
-          setArtists(res.data.acf.artists)
-        }
-        if (res.data.acf.hero) {
-          setHero(res.data.acf.hero)
-        }
-      }
-    })
-  }
-
   useEffect(() => {
+    const fetchData = () => {
+      axios.get(`${getApiURL()}/wp-json/wp/v2/pages/${pageId}`).then(res => {
+        if (res.data && res.data.acf) {
+          if (res.data.acf.artists) {
+            setArtists(res.data.acf.artists)
+          }
+          if (res.data.acf.hero) {
+            setHero(res.data.acf.hero)
+          }
+        }
+      })
+    }
+
     fetchData()
-  }, [])
+  }, [pageId])
 
   return children({ asyncHero: hero, asyncArtists: artists })
 }
