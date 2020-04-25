9f3df86fb029d23412c77d5c7efdb9ed865f54aa
mittnavnermike <niiimon@gmail.com>
2020-04-25 21:10:51 +0200

    added fix for no artists

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -311,92 +311,92 @@ export const query = graphql`
               alt
             }
           }
-          featured_image {
-            wordpress_id
-            title
-            url
-            alt
-            description
-            caption
-            name
-            mime_type
-            subtype
-            width
-            height
-            sizes {
-              thumbnail
-              thumbnail_width
-              thumbnail_height
-              medium
-              medium_width
-              medium_height
-              medium_large
-              medium_large_width
-              medium_large_height
-              large
-              large_width
-              large_height
-              small
-              small_width
-              small_height
-              medium_small
-              medium_small_width
-              medium_small_height
-              xlarge
-              xlarge_width
-              xlarge_height
-            }
-          }
+          # featured_image {
+          #   wordpress_id
+          #   title
+          #   url
+          #   alt
+          #   description
+          #   caption
+          #   name
+          #   mime_type
+          #   subtype
+          #   width
+          #   height
+          #   sizes {
+          #     thumbnail
+          #     thumbnail_width
+          #     thumbnail_height
+          #     medium
+          #     medium_width
+          #     medium_height
+          #     medium_large
+          #     medium_large_width
+          #     medium_large_height
+          #     large
+          #     large_width
+          #     large_height
+          #     small
+          #     small_width
+          #     small_height
+          #     medium_small
+          #     medium_small_width
+          #     medium_small_height
+          #     xlarge
+          #     xlarge_width
+          #     xlarge_height
+          #   }
+          # }
         }
       }
-      artists {
-        wordpress_id
-        status
-        title {
-          rendered
-        }
-        featured_image {
-          wordpress_id
-          title
-          url
-          alt
-          description
-          caption
-          name
-          mime_type
-          subtype
-          width
-          height
-          sizes {
-            thumbnail
-            thumbnail_width
-            thumbnail_height
-            medium
-            medium_width
-            medium_height
-            medium_large
-            medium_large_width
-            medium_large_height
-            large
-            large_width
-            large_height
-            small
-            small_width
-            small_height
-            medium_small
-            medium_small_width
-            medium_small_height
-            xlarge
-            xlarge_width
-            xlarge_height
-          }
-        }
-        acf {
-          country_code
-          greencopper_url
-          headliner
-        }
-      }
+      # artists {
+      #   wordpress_id
+      #   status
+      #   title {
+      #     rendered
+      #   }
+      #   featured_image {
+      #     wordpress_id
+      #     title
+      #     url
+      #     alt
+      #     description
+      #     caption
+      #     name
+      #     mime_type
+      #     subtype
+      #     width
+      #     height
+      #     sizes {
+      #       thumbnail
+      #       thumbnail_width
+      #       thumbnail_height
+      #       medium
+      #       medium_width
+      #       medium_height
+      #       medium_large
+      #       medium_large_width
+      #       medium_large_height
+      #       large
+      #       large_width
+      #       large_height
+      #       small
+      #       small_width
+      #       small_height
+      #       medium_small
+      #       medium_small_width
+      #       medium_small_height
+      #       xlarge
+      #       xlarge_width
+      #       xlarge_height
+      #     }
+      #   }
+      #   acf {
+      #     country_code
+      #     greencopper_url
+      #     headliner
+      #   }
+      # }
     }
   }
 `
