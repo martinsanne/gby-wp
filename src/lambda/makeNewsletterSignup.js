7ca392182549fc5178b42c11c52fe2e1bcdffd75
mittnavnermike <niiimon@gmail.com>
2021-07-08 13:11:19 +0200

    🐛 ♻️ Updated Make newsletter sign up implementation to new api

--- src/lambda/makeNewsletterSignup.js
+++ src/lambda/makeNewsletterSignup.js
@@ -1,33 +1,46 @@
-const request = require("request")
+// const request = require("request")
+const fetch = require("node-fetch").default
+const btoa = require("btoa")
 
 export function handler(event, context, callback) {
   const bodyJson = JSON.parse(event.body)
 
-  const formData = {
-    f: "2159",
-    k: "8858",
-    a: "on",
-    subscriber_list_ids: "35115",
-    email: bodyJson.email,
+  const User_id = "9874"
+  const Api_key = "024219c09741cff4d8ecec693876136bf1b8664e"
+
+  function authenticateUser(user, password) {
+    var token = user + ":" + password
+    var hash = btoa(token)
+
+    return "Basic " + hash
   }
 
-  request.post(
-    { url: "https://ext.mnm.as/subscribe.asp", form: formData },
-    (error, resp, body) => {
-      if (!error && body) {
-        // Will return body with redirect back to oyafestivalen.no
-        // Usually code 302, but let's not check that in case of change
-        console.log("Newsletter signup success")
+  // Create Auth token
+  const token = authenticateUser(User_id, Api_key)
+
+  fetch(
+    "https://subscribers.dialogapi.no/api/public/v2/subscribers?subscriber_list_id=9447",
+    {
+      body: JSON.stringify(bodyJson),
+      headers: {
+        Authorization: token,
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    }
+  )
+    .then(res => res.json())
+    .then(res => {
+      if (res.status === "200 Success" && res.id) {
         callback(null, {
           statusCode: 200,
           body: JSON.stringify({
-            resp,
-            formData,
+            res,
+            bodyJson,
           }),
         })
       } else {
         callback("Newsletter signup failed")
       }
-    }
-  )
+    })
 }
